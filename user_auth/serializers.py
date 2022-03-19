4705eef5d64d04a68d3d6a521578f9680682e1bf
scripthound <grinkiko@gmail.com>
2022-03-19 17:02:08 +0300

    [DELETION] added competency deletion

--- user_auth/serializers.py
+++ user_auth/serializers.py
@@ -1,11 +1,19 @@
 from rest_framework import serializers
 
 from user_auth.models import Profile
+from competitions.models import Competency
 from competitions.serializers import CompetencySerializer
 
 
+class CompetencySerializerWithID(CompetencySerializer):
+    class Meta:
+        model = Competency
+        depth = 1
+        fields = ['name', 'level', 'user', 'id']
+
+
 class ProfileSerializer(serializers.ModelSerializer):
-    competencies = CompetencySerializer(many=True, read_only=True)
+    competencies = CompetencySerializerWithID(many=True, read_only=True)
 
     class Meta:
         model = Profile
